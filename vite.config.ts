5abbfae351ea8a3ee5eb9ee1cb056fa30c545d55
gwt805 <1973735972@qq.com>
2023-12-23 22:34:18 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/gwt805.github.io/',
   build: {
     outDir: "docs"
   },
