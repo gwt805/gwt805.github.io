3d7702a7abab3bdebd16c41efde390adb3e888f1
gwt805 <1973735972@qq.com>
2023-12-25 10:54:36 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,15 +8,7 @@ export default defineConfig({
   build: {
     outDir: "docs"
   },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+  plugins: [vue()],
   resolve:{
     alias:{
       '@': resolve(__dirname,'./src')
