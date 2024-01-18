8753b9e03fdaa4ff2b97f7ad5aaae8448ab55574
gwt805 <1973735972@qq.com>
2024-01-18 22:27:03 +0800

    add pluginWebUpdateNotice

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import { webUpdateNotice } from "@plugin-web-update-notification/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,29 @@ export default defineConfig({
   build: {
     outDir: "docs"
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    webUpdateNotice({
+      versionType: "build_timestamp",
+      checkInterval: 1000,
+      checkOnWindowFocus: true,
+      locale: "zh_CN",
+      localeData: {
+        en_US: {
+          title: "📢 system update",
+          description: "System update, please refresh the page",
+          buttonText: "refresh",
+          dismissButtonText: "dismiss",
+        },
+        zh_CN: {
+          title: "📢 系统更新",
+          description: "有新版本发布, 请刷新页面",
+          buttonText: "好的",
+          dismissButtonText: "取消",
+        }
+      }
+    })
+  ],
   resolve:{
     alias:{
       '@': resolve(__dirname,'./src')
