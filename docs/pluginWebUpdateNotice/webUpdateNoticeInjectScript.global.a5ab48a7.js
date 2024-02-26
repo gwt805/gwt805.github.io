8195ba42732ff460def860522fd55c4f4ffef645
gwt805 <1973735972@qq.com>
2024-02-26 10:47:59 +0800

    add web ui

--- docs/pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.a5ab48a7.js
+++ docs/pluginWebUpdateNotice/webUpdateNoticeInjectScript.global.a5ab48a7.js
@@ -16,5 +16,5 @@
 
   window.__checkUpdateSetup__({"versionType":"build_timestamp","checkInterval":1000,"checkOnWindowFocus":true,"locale":"zh_CN","localeData":{"en_US":{"title":"📢 system update","description":"System update, please refresh the page","buttonText":"refresh","dismissButtonText":"dismiss"},"zh_CN":{"title":"📢 系统更新","description":"有新版本发布, 请刷新页面","buttonText":"好的","dismissButtonText":"取消"}},"injectFileBase":"/"});
       ;const logFn = function pt(t,s){console.log(`version: %c${t}`,"color: #1677ff"),console.log(`release time: %c${new Date(s).toLocaleString()}`,"color: #1677ff")}
-      ;logFn('1707468575533', 1707468575581)
+      ;logFn('1708915651039', 1708915651075)
     
